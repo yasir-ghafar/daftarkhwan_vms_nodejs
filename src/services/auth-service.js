a119e27966f3c9298a57e17a53fdccc51e72fe14
yasir-ghafar <yasir896@gmail.com>
2025-08-13 16:50:58 +0500

    get all users response updated.

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,67 +1,68 @@
-const { StatusCodes } = require('http-status-codes');
-const { sequelize, Wallet, Company, Location } = require('../models');
+const { StatusCodes } = require("http-status-codes");
+const { sequelize, Wallet, Company, Location } = require("../models");
 
-const argon2 = require('argon2');
-const jwt = require('jsonwebtoken')
+const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
 
-const { AuthRepository } = require('../repositories');
-const AppError = require('../utils/error/app-error');
-const { ServerConfig } = require('../config');
-
-
+const { AuthRepository } = require("../repositories");
+const AppError = require("../utils/error/app-error");
+const { ServerConfig } = require("../config");
 
 const authRepository = new AuthRepository();
 
 async function createUser(userData) {
-    const transaction = await sequelize.transaction();
-    try {
+  const transaction = await sequelize.transaction();
+  try {
+    const hashedPassword = await hashPassword(userData.password);
 
-        const hashedPassword = await hashPassword(userData.password);
+    const user = await authRepository.create(
+      {
+        ...userData,
+        password_hash: hashedPassword,
+      },
+      { transaction }
+    );
 
+    // if user rols is a member, create a wallet
 
-        const user = await authRepository.create({
-            ...userData,
-            password_hash: hashedPassword
-        }, { transaction});
-
-        // if user rols is a member, create a wallet
-
-        if (user.role === 'member') {
-            await Wallet.create({
-                user_id: user.id,
-                balance: 50.00
-            }, { transaction });
-        }
-
-        await transaction.commit();
-
-        const safeUser = { ...user.dataValues };
-        delete safeUser.password_hash;
-
-        return safeUser;
-
-    } catch(error) {
-        if (error.name == 'SequelizeValidationError') {
-            let explanation = [];
-            error.errors.array.array.forEach((err) => {
-                explanation.push(err.message);
-            });
-            console.log(explanation);
-            throw new AppError('Cannot create a new User object', StatusCodes.INTERNAL_SERVER_ERROR);
-        }
-        throw error;
+    if (user.role === "member") {
+      await Wallet.create(
+        {
+          user_id: user.id,
+          balance: 50.0,
+        },
+        { transaction }
+      );
     }
+
+    await transaction.commit();
+
+    const safeUser = { ...user.dataValues };
+    delete safeUser.password_hash;
+
+    return safeUser;
+  } catch (error) {
+    if (error.name == "SequelizeValidationError") {
+      let explanation = [];
+      error.errors.array.array.forEach((err) => {
+        explanation.push(err.message);
+      });
+      console.log(explanation);
+      throw new AppError(
+        "Cannot create a new User object",
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+    throw error;
+  }
 }
 
 async function checkUserAlreadyExists(email) {
-    console.log(`Checking Email: ${email}`)
-    const user = await authRepository.getByEmail(email);
-    if (user) 
-        return true;
-    else 
-        return false;
+  console.log(`Checking Email: ${email}`);
+  const user = await authRepository.getByEmail(email);
+  if (user) return true;
+  else return false;
 }
-
 
 // async function loginUser(email, password) {
 //     try {
@@ -85,7 +86,7 @@ async function checkUserAlreadyExists(email) {
 //         console.log(password);
 //         console.log(user.password_hash);
 //         const isMatch = await comparePassword(user.password_hash, password);
-        
+
 //         if (!isMatch) {
 //             throw new AppError('Password is Incorrect', 400);
 //         }
@@ -95,7 +96,7 @@ async function checkUserAlreadyExists(email) {
 //             email: user.email,
 //             role: user.role
 //         })
-       
+
 //         console.log(token);
 
 //         const safeUser = { ...user.dataValues }
@@ -116,65 +117,67 @@ async function checkUserAlreadyExists(email) {
 // }
 
 async function loginUser(email, password) {
-    try {
-        const user = await authRepository.getByEmail(email);
+  try {
+    const user = await authRepository.getByEmail(email);
 
-        if (!user) {
-            throw new AppError('User Not Found!', 404);
-        }
-        
-        console.log(password);
-        console.log(user.password_hash);
-        const isMatch = await comparePassword(user.password_hash, password);
-        
-        if (!isMatch) {
-            throw new AppError('Password is Incorrect', 400);
-        }
-        const token = issueToken({
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            role: user.role
-        })
-       
-        console.log(token);
-
-        const safeUser = { ...user.dataValues }
-        delete safeUser.password_hash;
-
-        return {...safeUser,
-             authorization: token};
-    } catch(error) {
-      console.log(error);
-        await transaction.rollback();
-        if (error.name == 'SequelizeValidationError') {
-            let explanation = error.errors.map(err => err.message);
-            console.log(explanation);
-            throw new AppError('Cannot create a new User object', StatusCodes.INTERNAL_SERVER_ERROR);
-        }
-        throw error;
+    if (!user) {
+      throw new AppError("User Not Found!", 404);
     }
+
+    console.log(password);
+    console.log(user.password_hash);
+    const isMatch = await comparePassword(user.password_hash, password);
+
+    if (!isMatch) {
+      throw new AppError("Password is Incorrect", 400);
+    }
+    const token = issueToken({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
+
+    console.log(token);
+
+    const safeUser = { ...user.dataValues };
+    delete safeUser.password_hash;
+
+    return { ...safeUser, authorization: token };
+  } catch (error) {
+    console.log(error);
+    await transaction.rollback();
+    if (error.name == "SequelizeValidationError") {
+      let explanation = error.errors.map((err) => err.message);
+      console.log(explanation);
+      throw new AppError(
+        "Cannot create a new User object",
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+    throw error;
+  }
 }
 
 async function hashPassword(password) {
-    return await  argon2.hash(password);
+  return await argon2.hash(password);
 }
 
 async function comparePassword(hashedPassword, password) {
-    console.log("Passowrd Hashed", hashedPassword)
-    console.log("Passowrd", password)
-    try{
-        return await argon2.verify(hashedPassword, password);
-    } catch(error) {
-        console.log(`Error in comparing password: ${error}`)
-    }
+  console.log("Passowrd Hashed", hashedPassword);
+  console.log("Passowrd", password);
+  try {
+    return await argon2.verify(hashedPassword, password);
+  } catch (error) {
+    console.log(`Error in comparing password: ${error}`);
+  }
 }
 
 async function getUserProfile(userId) {
-    try {
-        const user = await authRepository.get(userId);
-        return user;
-    } catch (error) {
+  try {
+    const user = await authRepository.get(userId);
+    return user;
+  } catch (error) {
     if (error.name == "SequelizeValidationError") {
       let explanation = [];
       error.errors.array.forEach((err) => {
@@ -190,12 +193,22 @@ async function getUserProfile(userId) {
   }
 }
 
-
 async function getAllUsers() {
-    try {
-        const users = await authRepository.getAll();
-        return users;
-    } catch (error) {
+  try {
+    const users = await authRepository.getAll({
+      include: [
+        {
+          model: Company,
+          attributes: ["id", "name", "LocationId", "locationName"],
+        },
+        {
+          model: Wallet,
+          attributes: ["id", "meeting_room_credits", "printing_credits"],
+        },
+      ],
+    });
+    return users;
+  } catch (error) {
     if (error.name == "SequelizeValidationError") {
       let explanation = [];
       error.errors.array.forEach((err) => {
@@ -212,12 +225,12 @@ async function getAllUsers() {
 }
 
 async function getAllUsersByCompanyId(company_id) {
-    try {
-        const users = await authRepository.getAll({
-            where: { company_id }
-        })
-         return users;
-    } catch (error) {
+  try {
+    const users = await authRepository.getAll({
+      where: { company_id },
+    });
+    return users;
+  } catch (error) {
     if (error.name == "SequelizeValidationError") {
       let explanation = [];
       error.errors.array.forEach((err) => {
@@ -233,9 +246,8 @@ async function getAllUsersByCompanyId(company_id) {
   }
 }
 
-
 function issueToken(payload) {
-  return jwt.sign(payload, ServerConfig.JWT_SECRET, { expiresIn: '12h' });
+  return jwt.sign(payload, ServerConfig.JWT_SECRET, { expiresIn: "12h" });
 }
 
 async function verifyToken(token) {
@@ -243,12 +255,11 @@ async function verifyToken(token) {
 }
 
 module.exports = {
-    createUser,
-    loginUser,
-    checkUserAlreadyExists,
-    verifyToken,
-    getUserProfile,
-    getAllUsers,
-    getAllUsersByCompanyId
-    
-}
+  createUser,
+  loginUser,
+  checkUserAlreadyExists,
+  verifyToken,
+  getUserProfile,
+  getAllUsers,
+  getAllUsersByCompanyId,
+};
