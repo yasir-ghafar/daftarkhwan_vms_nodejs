7b2104486b921a0ceebc8725a7ad0c49b4075281
yasir-ghafar <yasir896@gmail.com>
2025-08-26 20:58:00 +0500

    company routes updated.

--- src/routes/v1/company-routes.js
+++ src/routes/v1/company-routes.js
@@ -8,16 +8,18 @@ router.get('/wallet/transactions/:id', CompanyController.getWalletTransactions);
 
 router.put('/wallets/:id', CompanyController.updateWalletCredits);
 
-// router.post('/', CompanyController.createCompany);
+router.post('/', CompanyController.createCompany);
 
-// router.get('/location-id/:id', CompanyController.getCompaniesByLocationId);
+router.get('/location-id/:id', CompanyController.getCompaniesByLocationId);
 
-// // keep generic routes at the end
-// router.get('/:id', CompanyController.getCompanyById);
+// keep generic routes at the end
+router.get('/:id', CompanyController.getCompanyById);
 
-// router.delete('/delete', CompanyController.deletCompany);
+router.get('/', CompanyController.getCompanies);
 
-// router.put('/update-status', CompanyController.updateCompanyStatus);
+router.delete('/delete', CompanyController.deletCompany);
+
+router.put('/update-status', CompanyController.updateCompanyStatus);
 
 
 
