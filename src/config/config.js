a6f6ee055869c1db4c7ed79d2c291f018c65659d
Yasir Ghafar <yasir896@gmail.com>
2025-08-28 03:11:58 +0500

    Update config.js
    Changed the production block to match with the .env file.

--- src/config/config.js
+++ src/config/config.js
@@ -17,9 +17,9 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
   }
