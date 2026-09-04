c1b5d5b621e1b0d8a10687d872bc3e3aa6418799
yasir-ghafar <yasir896@gmail.com>
2026-09-04 11:38:11 +0500

    config.js file refactored.

--- src/config/config.js
+++ src/config/config.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: "root",
-    password: "pakistan1947",
-    database: "daftarkhwan_new_db",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
+    host: process.env.DB_HOST,
     dialect: "mysql"
   },
   test: {
